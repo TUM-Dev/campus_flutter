8f14352a1fb5aa2ff04107849ebd5b9e4093c184
Jakob Körber <jakobkoerber@posteo.de>
2023-05-14 18:22:43 +0200

    updated design to use cards

--- lib/gradeComponent/viewModels/gradeViewModel.dart
+++ lib/gradeComponent/viewModels/gradeViewModel.dart
@@ -2,14 +2,17 @@ import 'package:campus_flutter/base/networking/protocols/api.dart';
 import 'package:campus_flutter/gradeComponent/model/grade.dart';
 import 'package:campus_flutter/gradeComponent/services/gradeService.dart';
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 
-class GradeViewModel extends ChangeNotifier {
+class GradeViewModel {
   APIState<List<Grade>> state = const APIState.na();
   bool hasError = false;
-  Map<String, Map<String, List<Grade>>> _grades = {};
+  //final ValueNotifier<Map<String, Map<String, List<Grade>>>> _grades = ValueNotifier({});
+  BehaviorSubject<Map<String, Map<String, List<Grade>>>> _grades =
+  new BehaviorSubject<Map<String, Map<String, List<Grade>>>>.seeded({});
 
   Future<Map<String, Map<String, List<Grade>>>>
-      gradesByDegreeAndSemester() async {
+  gradesByDegreeAndSemester() async {
     List<Grade> grades = await GradeService.fetchGrades();
 
     if (grades.isEmpty) {
@@ -31,12 +34,12 @@ class GradeViewModel extends ChangeNotifier {
       }
     }
 
-    _grades = gradesByDegreeAndSemester;
+    _grades.value = gradesByDegreeAndSemester;
     return gradesByDegreeAndSemester;
   }
 
   Map<double, int> chartDataForDegree(String studyID) {
-    final degreeGrades = _grades[studyID];
+    final degreeGrades = _grades.value[studyID];
     if (degreeGrades == null) {
       return {};
     }
@@ -47,7 +50,7 @@ class GradeViewModel extends ChangeNotifier {
         final gradeValue = grade.grade ?? 0.0;
         chartData.update(
           gradeValue,
-          (value) => ++value,
+              (value) => ++value,
           ifAbsent: () => 1,
         );
       }
@@ -59,33 +62,47 @@ class GradeViewModel extends ChangeNotifier {
 
   static Color getColor(double? grade) {
     if (grade != null) {
-      if (grade! >= 1.0 && grade! < 1.4) {
+      if (grade >= 1.0 && grade < 1.4) {
         return const Color.fromRGBO(87, 159, 43, 1.0);
-      } else if (grade! >= 1.4 && grade! < 1.8) {
+      } else if (grade >= 1.4 && grade < 1.8) {
         return const Color.fromRGBO(119, 195, 68, 1.0);
-      } else if (grade! >= 1.8 && grade! < 2.1) {
+      } else if (grade >= 1.8 && grade < 2.1) {
         return const Color.fromRGBO(149, 210, 107, 1.0);
-      } else if (grade! >= 2.1 && grade! < 2.4) {
+      } else if (grade >= 2.1 && grade < 2.4) {
         return const Color.fromRGBO(255, 247, 125, 1.0);
-      } else if (grade! >= 2.4 && grade! < 2.8) {
+      } else if (grade >= 2.4 && grade < 2.8) {
         return const Color.fromRGBO(255, 238, 117, 1.0);
-      } else if (grade! >= 2.8 && grade! < 3.0) {
+      } else if (grade >= 2.8 && grade < 3.0) {
         return const Color.fromRGBO(255, 223, 0, 1.0);
-      } else if (grade! >= 3.0 && grade! < 3.4) {
+      } else if (grade >= 3.0 && grade < 3.4) {
         return const Color.fromRGBO(247, 199, 88, 1.0);
-      } else if (grade! >= 3.4 && grade! < 3.8) {
+      } else if (grade >= 3.4 && grade < 3.8) {
         return const Color.fromRGBO(245, 180, 51, 1.0);
-      } else if (grade! >= 3.8 && grade! < 4.0) {
+      } else if (grade >= 3.8 && grade < 4.0) {
         return const Color.fromRGBO(243, 175, 34, 1.0);
-      } else if (grade! == 4.0) {
+      } else if (grade == 4.0) {
         return const Color.fromRGBO(248, 137, 0, 1.0);
-      } else if (grade! >= 4.1 && grade! <= 5.0) {
+      } else if (grade >= 4.1 && grade <= 5.0) {
         return const Color.fromRGBO(236, 60, 26, 1.0);
       } else {
         return const Color.fromRGBO(205, 205, 205, 1.0);
       }
     } else {
       return const Color.fromRGBO(205, 205, 205, 1.0);
+    }
+  }
+
+  static String toFullSemesterName(String semester) {
+    final year = "20${semester.substring(0, 2)}";
+    final nextYearShort = (int.parse(year) + 1).toString().substring(2, 4);
+
+    switch (semester.substring(2)) {
+      case "W":
+        return "Wintersemester" " $year/$nextYearShort";
+      case "S":
+        return "Summersemester" " $year";
+      default:
+        return "Unknown";
     }
   }
 }
---
+++ lib/lectureComponent/views/lectureDetailsView.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LectureDetailsView extends StatelessWidget {
+  const LectureDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Test"),
+      ),
+    );
+  }
+}
