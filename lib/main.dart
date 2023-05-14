61626de02cf2010e3566522793fb5d7cb36f9e85
Jakob Körber <jakobkoerber@posteo.de>
2023-05-14 18:06:42 +0200

    theme updates

--- lib/main.dart
+++ lib/main.dart
@@ -24,17 +24,53 @@ class CampusApp extends StatelessWidget {
         theme: ThemeData(
             brightness: Brightness.light,
             colorSchemeSeed: const Color(0xff0065bd),
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            scaffoldBackgroundColor: const Color(0xfff2f2f7),
+            appBarTheme: const AppBarTheme(
+                surfaceTintColor: Color(0xfff2f2f7),
+                backgroundColor: Color(0xfff2f2f7)),
+            cardTheme: const CardTheme(
+                color: Colors.white,
+                surfaceTintColor: Colors.transparent,
+                shadowColor: Colors.transparent),
+            expansionTileTheme: const ExpansionTileThemeData(
+                collapsedTextColor: Color(0xff0065bd),
+                collapsedIconColor: Color(0xff0065bd)),
             useMaterial3: true,
+            dividerColor: Colors.transparent,
             navigationBarTheme: const NavigationBarThemeData(
-                indicatorColor: Colors.transparent
-            )),
+                indicatorColor: Colors.transparent,
+                backgroundColor: Color(0xF0F8F9F8),
+                surfaceTintColor: Colors.transparent,
+                elevation: 50.0)),
         darkTheme: ThemeData(
             brightness: Brightness.dark,
             colorSchemeSeed: const Color(0xff0065bd),
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            scaffoldBackgroundColor: const Color(0xff191919),
+            appBarTheme: const AppBarTheme(
+                surfaceTintColor: Color(0xff191919),
+                backgroundColor: Color(0xff191919)),
+            cardTheme: const CardTheme(
+                color: Color(0xff252525),
+                surfaceTintColor: Colors.transparent,
+                shadowColor: Colors.transparent),
+            expansionTileTheme: ExpansionTileThemeData(
+              // TODO: figure out color for collapsed text
+              collapsedTextColor:
+              Theme.of(context).expansionTileTheme.textColor,
+              collapsedIconColor:
+              Theme.of(context).expansionTileTheme.textColor,
+            ),
             useMaterial3: true,
+            dividerColor: Colors.transparent,
             navigationBarTheme: const NavigationBarThemeData(
-                indicatorColor: Colors.transparent
-            )),
+                indicatorColor: Colors.transparent,
+                backgroundColor: Color(0xF01D1D1D),
+                surfaceTintColor: Colors.transparent,
+                elevation: 50.0)),
         routes: {
           confirm: (context) => const ConfirmView(),
         },
