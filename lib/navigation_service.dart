55485b3454f7021af9047373445132e553ca304d
Jakob Körber <56073945+jakobkoerber@users.noreply.github.com>
2023-12-07 14:05:29 +0100

    fix linting issues

--- lib/navigation_service.dart
+++ lib/navigation_service.dart
@@ -5,7 +5,6 @@ import 'package:campus_flutter/gradeComponent/views/grades_view.dart';
 import 'package:campus_flutter/homeComponent/home_screen.dart';
 import 'package:campus_flutter/lectureComponent/views/lectures_view.dart';
 import 'package:campus_flutter/placesComponent/views/places_screen.dart';
-import 'package:campus_flutter/searchComponent/views/appWideSearch/search_scaffold.dart';
 import 'package:campus_flutter/settingsComponent/views/settings_scaffold.dart';
 import 'package:campus_flutter/base/extensions/context.dart';
 import 'package:flutter/foundation.dart';
@@ -77,12 +76,13 @@ class NavigationService {
   Widget searchButton(BuildContext context) {
     return IconButton(
       onPressed: () => throw Exception()
-          /*Navigator.push(
+      /*Navigator.push(
         context,
         MaterialPageRoute(
           builder: (context) => const SearchScaffold(),
         ),
-      )*/,
+      )*/
+      ,
       icon: const Icon(Icons.search),
     );
   }
