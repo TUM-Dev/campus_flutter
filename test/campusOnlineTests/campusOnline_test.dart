bf8912c40c91a6c94b5a579d73149b692fc9b8c1
Jakob Körber <56073945+jakobkoerber@users.noreply.github.com>
2024-03-28 19:58:57 +0100

    Fix Tests

--- test/campusOnlineTests/campusOnline_test.dart
+++ test/campusOnlineTests/campusOnline_test.dart
@@ -10,13 +10,16 @@ void main() {
 
 dynamic xml2JsonConverter(String xml) {
   final transformer = Xml2Json()..parse(xml);
-  final json = transformer.toParkerWithAttrsCustom(array: [
-    "row",
-    "event",
-    "studium",
-    "raeume",
-    "gruppen",
-    "telefon_nebenstellen",
-  ]);
+  final json = transformer.toParkerWithAttrs(
+    entries: {
+      "row": "rowset",
+      "event": "events",
+      "studium": "studien",
+      "raum": "raeume",
+      "gruppe": "gruppen",
+      "nebenstelle": "telefon_nebenstellen",
+      "card": "cards",
+    },
+  );
   return jsonDecode(json);
 }
