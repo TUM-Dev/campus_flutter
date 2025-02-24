5c2778787183c3d5c97b7e9faa4e16ba57a38eab
Jakob Körber <56073945+jakobkoerber@users.noreply.github.com>
2025-02-24 16:07:26 +0100

    Add Legend to Campus Map

--- lib/placesComponent/views/campuses/campus_map_view.dart
+++ lib/placesComponent/views/campuses/campus_map_view.dart
@@ -1,7 +1,10 @@
 import 'package:campus_flutter/base/enums/campus.dart';
+import 'package:campus_flutter/base/extensions/context.dart';
 import 'package:campus_flutter/base/util/custom_back_button.dart';
+import 'package:campus_flutter/base/util/icon_text.dart';
 import 'package:campus_flutter/placesComponent/viewModels/places_viewmodel.dart';
 import 'package:campus_flutter/placesComponent/views/map_widget.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -19,9 +22,7 @@ class CampusMapScaffold extends StatelessWidget {
         title: Text(campus.name),
         titleSpacing: 0,
       ),
-      body: CampusMapView(
-        campus: campus,
-      ),
+      body: CampusMapView(campus: campus),
     );
   }
 }
@@ -33,16 +34,61 @@ class CampusMapView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return MapWidget.noPadding(
-      aspectRatioNeeded: false,
-      markers: ref.read(placesViewModel).getCampusMarkers(context, campus),
-      latLng: LatLng(
-        campus.location.latitude,
-        campus.location.longitude,
+    return Stack(
+      alignment: Alignment.topRight,
+      children: [
+        MapWidget.noPadding(
+          aspectRatioNeeded: false,
+          markers: ref.read(placesViewModel).getCampusMarkers(context, campus),
+          latLng: LatLng(campus.location.latitude, campus.location.longitude),
+          zoom: 15,
+          roundedCorners: false,
+          controlPadding: const EdgeInsets.all(20),
+        ),
+        _mapLegend(context),
+      ],
+    );
+  }
+
+  Widget _mapLegend(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(context.padding),
+      child: Container(
+        decoration: BoxDecoration(
+          color: context.theme.canvasColor,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            if (context.theme.brightness == Brightness.light)
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 1.0,
+                offset: Offset(0, 2),
+              ),
+          ],
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(context.padding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconText(
+                iconData: Icons.place,
+                label: context.tr("cafeterias"),
+                iconColor: Colors.blue,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: context.halfPadding),
+              ),
+              IconText(
+                iconData: Icons.place,
+                label: context.tr("studyRooms"),
+                iconColor: Colors.red,
+              ),
+            ],
+          ),
+        ),
       ),
-      zoom: 15,
-      roundedCorners: false,
-      controlPadding: const EdgeInsets.all(20),
     );
   }
 }
