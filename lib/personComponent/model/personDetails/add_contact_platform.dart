4be30577660ae85e252eb1f86fed4d886296ba88
Julian Henrik Alber <alber.julian@outlook.com>
2025-07-31 08:57:04 +0200

    remove unnecessary code

--- lib/personComponent/model/personDetails/add_contact_platform.dart
+++ lib/personComponent/model/personDetails/add_contact_platform.dart
@@ -1,21 +1,8 @@
-import 'package:campus_flutter/personComponent/model/personDetails/add_contact_platform_interface.dart';
 import 'package:campus_flutter/personComponent/model/personDetails/person_details.dart';
 import 'package:flutter/services.dart';
 
 // this invokes the os specific ways of handling contacts
-final class AddContactPlatform extends PlatformInterface {
-
-  AddContactPlatform() : super(token: _token);
-
-  static final Object _token = Object();
-
-  static AddContactPlatform _instance = AddContactPlatform(); // TODO: Maybe remove, or also remove the "extends"
-
-  static AddContactPlatform get instance => _instance;
-  static set instance(AddContactPlatform instance) {
-    PlatformInterface.verifyToken(instance, _token);
-    _instance = instance;
-  }
+final class AddContactPlatform {
 
   static final methodChannel = const MethodChannel('add_contact');
 
