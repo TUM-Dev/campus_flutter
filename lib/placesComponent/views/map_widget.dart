b124c4239e29254705325cc033a03a32330b0875
Nathan <239961557+nathan01100010@users.noreply.github.com>
2025-11-22 22:09:08 +0100

    Pin now uses file instead of base64, changed to pin currently on NavigaTUM instead of the high resolution one because it was off center and this is good enough

--- lib/placesComponent/views/map_widget.dart
+++ lib/placesComponent/views/map_widget.dart
@@ -1,15 +1,12 @@
 import 'dart:async';
-import 'dart:convert';
 import 'package:campus_flutter/base/extensions/context.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:maplibre_gl/maplibre_gl.dart' as maplibre;
-
-const pinBase64 = "UklGRroCAABXRUJQVlA4TK0CAAAvGMAIEPfkoI0kR6rZ+wDg+cN6JJfDdn0g4biRJEWKPfie/z4y38J0l+NIkhwlZw/NCzfw3wGM0uxOw61tW1U1cSeiAUL6L4AmiNzdvuvlngRI0XgaSBgIBjts0mFFGsI5t1T0rdkwlXOO8z+ZQkBLFbvZkeL4LenhmqdUZIQ29nStgzUZ5utfRqSDkIq+xTFv+vqVEanICEMqhuPReJ5q+upbENJBMJiD49/a9MVUauKcmx0pMqKPyIhUENrxbHsvx2+x32zsMBAMJAwQEJAhIEH8IUMWpQrQkUBHUYJCcVb9QJInJHhBipDgB/UofpGQQL5QH8W3KiMf8IP6apJLdWGJ+dOM99K7wXq9WZ2ZX1y3coAbkosyaiyfkhr/SkS9o+aNcjGSj7KdYre/GOB77H7VG/1m9Aa34fP0/Hy9uoFHersXoQqJkiSbttV7nWvb1rNt+13Vs23buM++Nn521l7nRLwfiOj/BMgfvfl++hpHT9x+Nq3wv5+Jv/p4JmDsC+HnR2KGzmI7d67ffshwZdATXQfY0ZjtnMus2whw6qN5BPQuSHex7Z3AXUmfjgIdLrSpFxiXHgIbEkFuGXBXOg9Uu/DCLjiuSWC3s6WbDm6uMG4tMDgBLDNZ+4eHhw/kmYXAq35gvikftvWmHXj6EphnSjxVpgX49RZYatyq4eHh1QnTATyfA3Z5Es2LWzOcXQMM6BxQaQILDsOlSD+AdUksAb5JY0eBhqCybuC9pAfAvpyAtC3AV0maPQksCmgDrvUZvQZ6amOKpoDf8t8C9uR60rcBF6OYvmPACs984MiA4vsBWpxzNV3AE4XeA6aKXf5e4IaCp88AW9OWA8c+hGngCLAS4J+S/Yn/jpKOLngujyand54XSuV94KZSOn2CoyOp0SueKNV/9J8DAA==";
-// TODO: include as file instead of this nonsense - Nathan
 
 class MapWidget extends ConsumerStatefulWidget {
   factory MapWidget.fullPadding({
@@ -147,10 +144,7 @@ class _MapWidgetState extends ConsumerState<MapWidget> {
         alignment: Alignment.topRight,
         children: [
           maplibre.MapLibreMap(
-            // styleString:
-            //     "https://nav.tum.de/martin/style/navigatum-basemap.json", // TODO: dark and light themes - Nathan
-            // mapType: MapType.normal,
-            // padding: widget.controlPadding ?? EdgeInsets.zero,
+            styleString: "https://nav.tum.de/martin/style/navigatum-basemap.json", // TODO: dark and light themes - Nathan
             initialCameraPosition: maplibre.CameraPosition(
               target:
                   // mapLibre and Google Maps have different latLng classes. This converts between them and also provides a default location. - Nathan
@@ -167,9 +161,8 @@ class _MapWidgetState extends ConsumerState<MapWidget> {
             },
             rotateGesturesEnabled: false,
             compassEnabled: false,
-            //mapToolbarEnabled: false, ? no apparent equivalent
             tiltGesturesEnabled: false,
-            zoomGesturesEnabled: true, // zoomControlsEnabled
+            zoomGesturesEnabled: true,
             myLocationEnabled: true,
             myLocationRenderMode: maplibre.MyLocationRenderMode.compass,
             onMapCreated: (maplibre.MapLibreMapController controller) {
@@ -177,26 +170,26 @@ class _MapWidgetState extends ConsumerState<MapWidget> {
               // Previous 250ms mounted delay no longer needed as it is now handled by onStyleLoadedCallback
             },
 
-            //replacement for "markers: widget.markers". I know, it's a bit longer. Sorry! - Nathan
             onStyleLoadedCallback: () => {
-              _controller.future.then((controller) {
-                controller.addImage("pin", base64Decode(pinBase64));
-
+              _controller.future.then((controller) async {
+                final ByteData pinData = await rootBundle.load("assets/images/map/pin.webp");
+                controller.addImage("pin", pinData.buffer.asUint8List());
                 // Aligns better with Google Maps marker behavior
                 controller.symbolManager?.setIconAllowOverlap(true);
                 controller.symbolManager?.setIconIgnorePlacement(true);
                 controller.symbolManager?.setTextAllowOverlap(true);
                 controller.symbolManager?.setTextIgnorePlacement(true);
 
+                //replacement for "markers: widget.markers". I know, it's a bit longer. Sorry! - Nathan
                 for (var marker in widget.markers) {
                   controller.addSymbol(
                     maplibre.SymbolOptions(
                       iconSize: 3, // Not set by Google Markers, done by eye by comparing screenshots
                       iconImage: "pin", // Theoretically this could be set based on the marker ID. But in the current app it's always the same anyway.
                       iconRotate: marker.rotation,
-                      iconOffset: marker.anchor,
+                      iconOffset: Offset(0, -5), // end of the point of the arrow is on the exact location (magic number (done by eye))
                       textField: marker.infoWindow.title,
-                      textOffset: Offset(0, -2.5), // magic number (done by eye)
+                      textOffset: Offset(0, -3), // magic number (done by eye)
                       iconOpacity: marker.alpha,
                       geometry: maplibre.LatLng(
                         marker.position.latitude,
@@ -228,7 +221,7 @@ class _MapWidgetState extends ConsumerState<MapWidget> {
                     symbol,
                     maplibre.SymbolOptions(textOpacity: 1),
                   );
-                  controller.moveCamera(maplibre.CameraUpdate.newLatLng(symbol.options.geometry ?? maplibre.LatLng(0, 0))); // something will have gone very wrong if it goes to 0,0 - Nathan
+                  controller.moveCamera(maplibre.CameraUpdate.newLatLng(symbol.options.geometry ?? maplibre.LatLng(0, 0))); // something will have gone very wrong if it goes to 0,0
                 });
 
                 if (mounted) {
