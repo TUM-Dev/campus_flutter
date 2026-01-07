da0e74a06da4ccb89d1c2fd7bdc31d15b3776e5e
Jakob Körber <56073945+jakobkoerber@users.noreply.github.com>
2026-01-07 15:52:57 +0100

    Update Deps

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,9 +19,9 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.13.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.21" apply  false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
+    id("com.android.application") version "8.13.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply  false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
     id("com.google.firebase.crashlytics") version "3.0.6" apply false
 }
