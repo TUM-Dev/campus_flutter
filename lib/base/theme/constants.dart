ae1283fae36f34ee0af9e31684eb56646e45768d
Kordian Bruck <kordianbruck@users.noreply.github.com>
2026-03-29 13:27:28 +0200

    Apply suggestion from @FlorianDev
    Co-authored-by: Florian Schmidt <FlorianDev@users.noreply.github.com>

--- lib/base/theme/constants.dart
+++ lib/base/theme/constants.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 const Color primaryLightColor = Color(0xff0064BC);
-//const Color primaryDarkColor = Color(0xff3070B3);
 const Color primaryDarkColor = Color.fromARGB(255, 100, 160, 200);
 const Color lightBackground = Color(0xfff2f2f7);
 const Color darkBackground = Color(0xff191919);
