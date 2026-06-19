e89cf3071e2838aa94ece1b061d1c981787a026e
nico <nicolas.popken@tum.de>
2026-06-19 17:08:20 +0200

    fix: change rss events feed with user language

--- lib/campusComponent/service/heilbronn_event_service.dart
+++ lib/campusComponent/service/heilbronn_event_service.dart
@@ -2,12 +2,14 @@ import 'package:campus_flutter/campusComponent/model/heilbronn_event.dart';
 import 'package:dio/dio.dart';
 
 class HeilbronnEventService {
-  static const feedUrl = 'https://hn.fs.tum.de/events/index.xml';
+  static String feedUrl(String languageCode) => languageCode == 'de'
+      ? 'https://hn.fs.tum.de/de/events/index.xml'
+      : 'https://hn.fs.tum.de/events/index.xml';
 
   static final _dio = Dio();
 
-  static Future<List<HeilbronnEvent>> fetchEvents() async {
-    final response = await _dio.get<String>(feedUrl);
+  static Future<List<HeilbronnEvent>> fetchEvents(String languageCode) async {
+    final response = await _dio.get<String>(feedUrl(languageCode));
     return HeilbronnEvent.listFromRss(response.data ?? '');
   }
 }
